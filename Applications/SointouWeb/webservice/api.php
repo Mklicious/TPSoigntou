a501aecbbdff28d1e1b421a19b83950ddacf50e4
Mklicious <mickael.doranges@gmail.com>
2016-04-11 10:47:50 -0400

    Modification de mk - il est content

--- Applications/SointouWeb/webservice/api.php
+++ Applications/SointouWeb/webservice/api.php
@@ -1,8 +1,51 @@
 <?php
-/*
- * @package WebService
- */
 
- // TO DO
+	/*
+	 * @package WebService
+	 */
 
-?>
+	$jurl = file_get_contents('http://data.iledefrance.fr/explore/dataset/carte-des-pharmacies-de-seine-et-marne/download?format=json'); //url du Json , il est content.
+
+	$json_obj = json_decode($jurl); //Rendons le lisible
+
+	$tab=array();
+
+
+	  //suppression des objets, il est content.
+
+	foreach($json_obj as $obj)
+		{
+
+	  		unset($obj->datasetid);
+
+	  		unset($obj->recordid);
+
+	  		unset($obj->geometry);
+
+	  		unset($obj->record_timestamp);
+
+	  		unset($obj->fields->departement);
+
+	  		unset($obj->fields->libdepartement);
+
+	  		unset($obj->fields->wgs);
+
+
+	  		$tab[]=$obj;
+
+		}
+
+
+
+
+
+ 	$json = json_encode($tab); //encodage json_obj
+	    
+	header('Content-Type: application/json');
+
+	echo $json;//affichage du json
+
+
+
+
+
